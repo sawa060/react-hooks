f65b6d263ad78ef2a54c75db006dff8ed9e6c9b4
澤田優作 <>
2020-09-28 17:29:13 +0900

    template

--- src/App.js
+++ src/App.js
@@ -1,52 +1,11 @@
 import React, { useState } from 'react'
 
 const App = () => {
-  const [count, setCount] = useState(0)
-
-  const increment = () => {
-    setCount(count + 1)
-  }
-  const decrement = () => {
-    setCount(count - 1)
-  }
-
-  const increment2 = () => {
-    setCount(prevCount => prevCount + 1)
-  }
-
-  const decrement2 = () => {
-    setCount(prevCount => prevCount - 1)
-  }
-
-  const reset = () => {
-    setCount(0)
-  }
-
-  const multiply = () => {
-    setCount(prevCount => prevCount * 2)
-  }
-
-  const devision3 = () => {
-    if (count % 3 === 0) {
-      setCount(prevCount => prevCount / 3)
-    } else return
-  }
 
   return (
     <>
       <div>
-        count: {count}
-      </div>
-      <button onClick={increment}> +1 </button>
-      <button onClick={decrement}> -1 </button>
-      <div>
-        <button onClick={increment2}> +1 </button>
-        <button onClick={decrement2}> -1 </button>
-      </div>
-      <div>
-        <button onClick={reset}> Reset </button>
-        <button onClick={multiply}> x2 </button>
-        <button onClick={devision3}> 3の倍数の時だけ3で割る </button>
+        this is a template
       </div>
     </>
     );
